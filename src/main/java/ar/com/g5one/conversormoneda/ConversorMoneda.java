415f9e5ca5a23d2f3d04e44adf2c5799a7c34713
Nadia Galarza <nadigarza1@gmail.com>
2023-08-12 12:50:05 -0300

    creacion repositorio

---
+++ src/main/java/ar/com/g5one/conversormoneda/ConversorMoneda.java
@@ -0,0 +1,17 @@
+
+
+package ar.com.g5one.conversormoneda;
+
+import ar.com.g5one.conversormoneda.view.JFrameAplication;
+
+/**
+ *
+ * @author nadia
+ */
+public class ConversorMoneda {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+         JFrameAplication app = new JFrameAplication();
+    }
+}
